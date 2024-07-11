00add3a646b1e1b0a1b19adc215d4197501f2fc7
charlescasemiro <49919162+charlescasemiro@users.noreply.github.com>
2024-07-11 01:38:13 +0100

    update

--- app/app-ci/assets/scripts-e-automatizadores/clone-modules.sh
+++ app/app-ci/assets/scripts-e-automatizadores/clone-modules.sh
@@ -7,7 +7,7 @@ cd /sei-modulos
 git clone https://github.com/pengovbr/mod-sei-estatisticas.git
 git clone https://github.com/pengovbr/mod-sei-pen.git
 git clone https://github.com/pengovbr/mod-wssei.git
-git clone https://github.com/anatelgovbr/mod-sei-peticionamento.git peticionamento
+git clone https://github.com/anatelgovbr/mod-sei-peticionamento.git
 git clone https://github.com/pengovbr/mod-sei-resposta.git
 git clone https://github.com/pengovbr/mod-sei-protocolo-integrado.git
 git clone https://${GITUSER_REPO_MODULOS}:${GITPASS_REPO_MODULOS}@github.com/pengovbr/mod-sei-incom.git
